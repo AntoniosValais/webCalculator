bdf8c1243602cdc3af46ddaa017ccc8348ad88d1
iKetsi <pketsentsidis@gmail.com>
2015-10-26 22:07:34 +0200

    updates on css/ui

--- calc101/jscalc.js
+++ calc101/jscalc.js
@@ -1,17 +1,6 @@
 /**
  * Created by Neo on 10/25/2015.
  */
-var operators = ['*' , '/' , '+' , '-'];
-
-/*var input = function(x){
-if(x=="Clear"){
-    document.Calc.Screen.value="";
-}
-else
-    document.Calc.Screen.value += x;
-};
-*/
-
 var numb1,numb2,operFlag;
 var calculate = function(numb1,numb2,operFlag){
     var result;
@@ -30,41 +19,40 @@ var calculate = function(numb1,numb2,operFlag){
     return result;
 }
 
-var newinput = function(x) {
+var input = function(x) {
     if (x == 'C') {
-        document.Calc.Screen.value = "";
+        document.getElementById("screen").innerHTML = "";
     }
     else if (x == '+') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
+        numb1 = document.getElementById("screen").innerHTML;
+        document.getElementById("screen").innerHTML = x;
         operFlag = '+';
     }
     else if (x == '-') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
+        numb1 = document.getElementById("screen").innerHTML;
+        document.getElementById("screen").innerHTML = x;
         operFlag = '-';
     }
     else if (x == '*') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
+        numb1 = document.getElementById("screen").innerHTML;
+        document.getElementById("screen").innerHTML = x;
         operFlag = '*';
     }
     else if (x == '/') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
+        numb1 = document.getElementById("screen").innerHTML;
+        document.getElementById("screen").innerHTML = x;
         operFlag = '/';
     }
     else if (x == '=') {
-        numb2 = document.Calc.Screen.value;
+        numb2 = document.getElementById("screen").innerHTML;
         result = calculate(numb1, numb2, operFlag);
-        document.Calc.Screen.value = result;
+        document.getElementById("screen").innerHTML = result;
     }
-
-    else if (x == '0', '1', '2', '3', '4', '5', '6', '7', '8', '9') {
-        if(document.Calc.Screen.value !== '*' || '+' || '-' || '/' || '=')
-        {document.Calc.Screen.value += x;}
+    else if (x == '0' || x == '1'|| x == '2'|| x == '3'|| x == '4'|| x == '5'|| x == '6'|| x == '7'|| x == '8'|| x == '9') {
+        if(document.getElementById("screen").innerHTML !== '*' || '+' || '-' || '/' || '=')
+        {document.getElementById("screen").innerHTML += x;}
         else
-        document.Calc.Screen.value = x;
+            document.getElementById("screen").innerHTML += x;
     }
 
 };
