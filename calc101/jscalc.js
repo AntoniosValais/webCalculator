efcd2a43797c49b2058ec0b9510ddc701f5c2f30
TonyVkoz <toniovkozz@gmail.com>
2015-10-26 17:26:56 +0200

    controller 0.1

--- calc101/jscalc.js
+++ calc101/jscalc.js
@@ -1,70 +1,192 @@
 /**
  * Created by Neo on 10/25/2015.
  */
-var operators = ['*' , '/' , '+' , '-'];
 
-/*var input = function(x){
-if(x=="Clear"){
-    document.Calc.Screen.value="";
-}
-else
-    document.Calc.Screen.value += x;
-};
-*/
+var operationOrder = ['*','/','+','-'];
+var insertedExpression = [];
+var insertedExpressionIndex = -1;
+var number = new Number();
 
-var numb1,numb2,operFlag;
-var calculate = function(numb1,numb2,operFlag){
-    var result;
-    if(operFlag ==('+')){
-        result = numb1 + numb2;
-    }
-    else if(operFlag ==('-')){
-        result = numb1 - numb2;
-    }
-    else if(operFlag ==('*')){
-        result = numb1 * numb2;
-    }
-    else if(operFlag ==('/')){
-        result = numb1 / numb2;
-    }
-    return result;
+function clearExpression()
+{
+    insertedExpression = [];
+    insertedExpressionIndex = -1;
+    number = new Number();
 }
 
-var newinput = function(x) {
-    if (x == 'C') {
-        document.Calc.Screen.value = "";
-    }
-    else if (x == '+') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
-        operFlag = '+';
-    }
-    else if (x == '-') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
-        operFlag = '-';
-    }
-    else if (x == '*') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
-        operFlag = '*';
-    }
-    else if (x == '/') {
-        numb1 = document.Calc.Screen.value;
-        document.Calc.Screen.value = x;
-        operFlag = '/';
-    }
-    else if (x == '=') {
-        numb2 = document.Calc.Screen.value;
-        result = calculate(numb1, numb2, operFlag);
-        document.Calc.Screen.value = result;
+function Number()
+{
+    this.totalNumber = 0.0;
+    this.insertingIntegerDigit = true;
+
+    this.lastDecimalDigitPosition = 1;
+
+    this.receiveIntegerDigit = function receiveIntegerDigit( receiveDecimalDigit )
+    {
+        this.insertingIntegerDigit = receiveDecimalDigit;
     }
 
-    else if (x == '0', '1', '2', '3', '4', '5', '6', '7', '8', '9') {
-        if(document.Calc.Screen.value !== '*' || '+' || '-' || '/' || '=')
-        {document.Calc.Screen.value += x;}
+    this.addLeastSignificantDigit = function addLeastSignificantDigit( digit )
+    {
+        if( this.insertingIntegerDigit )
+        {
+            this.addIntegerLeastSignificantDigit( digit );
+        }
         else
-        document.Calc.Screen.value = x;
+        {
+            this.addDecimalLeastSignificantDigit( digit );
+        }
     }
 
-};
+    this.addIntegerLeastSignificantDigit = function addIntegerLSD( integerLeastSignificantDigit )
+    {
+        this.totalNumber = this.totalNumber * 10 + integerLeastSignificantDigit;
+    }
+
+    this.addDecimalLeastSignificantDigit = function addDecimalLSD( decimalLeastSignificantDigit )
+    {
+        var decimalToBeAdded = decimalLeastSignificantDigit / Math.pow(10, this.lastDecimalDigitPosition);
+
+        this.totalNumber += decimalToBeAdded;
+
+        this.lastDecimalDigitPosition++;
+    }
+
+    this.getNumber = function getNumber()
+    {
+        return this.totalNumber;
+    }
+}
+
+function compute( argument1, argument2, operation )
+{
+    switch( operation )
+    {
+        case '*':
+
+            return argument1 * argument2;
+
+            break;
+
+        case '/':
+
+            if( argument2 == 0 )
+            {
+                return Math.POSITIVE_INFINITY;
+            }
+            else
+            {
+                return argument1 / argument2;
+            }
+
+            break;
+
+        case '+':
+
+            return argument1 + argument2;
+
+            break;
+
+        case '-':
+
+            return argument1 - argument2;
+
+            break;
+    }
+}
+
+function getExpressionResult()
+{
+    var operationIndex;
+    var element;
+
+    var argument1, argument2;
+
+    for( operationIndex = 0; operationIndex < operationOrder.length; operationIndex++ )
+{
+    var operation = operationOrder[ operationIndex ];
+
+    for( element = 0; element < insertedExpression.length; element++ )
+    {
+        if( insertedExpression[ element ] == operation )
+        {
+            argument1 = insertedExpression[ element - 1 ];
+            argument2 = insertedExpression[ element + 1 ];
+
+            var result = compute( argument1, argument2, operation );
+
+            insertedExpression.splice( (element-1), 3, result );
+
+            element--;
+        }
+    }
+}
+
+    return insertedExpression[0];
+}
+
+
+function newInput(element)
+{
+    var input = element.innerHTML;
+
+    switch (input)
+    {
+        case 'C':
+            clearExpression();
+            break;
+
+        case '.':
+
+            number.receiveIntegerDigit( false );
+
+            break;
+
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+
+            number.addLeastSignificantDigit( parseInt( input ) );
+
+            if ( isNaN( insertedExpression[ insertedExpressionIndex ] ) )
+            {
+                insertedExpression[ ++insertedExpressionIndex ] = number.getNumber();
+            }
+            else
+            {
+                insertedExpression[ insertedExpressionIndex ] = number.getNumber();
+            }
+
+            break;
+
+        case '*':
+        case '/':
+        case '+':
+        case '-':
+            number = new Number();
+
+            insertedExpression[ ++insertedExpressionIndex ] = input;
+            break;
+    }
+
+    if( input == '=' )
+    {
+        var result = getExpressionResult().toString();
+
+        clearExpression();
+
+        document.getElementById("screen").innerHTML = result;
+    }
+    else
+    {
+        document.getElementById("screen").innerHTML = " ";
+        document.getElementById("screen").innerHTML = insertedExpression.join("");
+    }
+}
