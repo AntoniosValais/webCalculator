1c7fd8e626d13cf355714cee67c663483df39613
iKetsi <pketsentsidis@gmail.com>
2015-10-27 21:52:11 +0200

    updated typo/extra buttons inc from happiness

--- calc101/expand.js
+++ calc101/expand.js
@@ -5,15 +5,15 @@ var expand = function() {
     var status = document.getElementById("shorten").style.display;
     if(status == "none"){
         document.getElementById("extras").style.display = "block";
-        document.getElementById("inner_container").style.height = "550px";
-        document.getElementById("breakline").style.display = "block";
+        document.getElementById("inner_container").style.height = "530px";
+        document.getElementById("breakLine").style.display = "block";
         document.getElementById("expand").style.display = "none";
         document.getElementById("shorten").style.display = "block";
     }
     else{
         document.getElementById("extras").style.display = "none";
         document.getElementById("inner_container").style.height = "";
-        document.getElementById("breakline").style.display = "none";
+        document.getElementById("breakLine").style.display = "none";
         document.getElementById("expand").style.display = "block";
         document.getElementById("shorten").style.display = "none";
     }
