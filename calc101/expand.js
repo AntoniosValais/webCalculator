1af1670f50b7cf456658ce4c4f21502c8c97fec9
iKetsi <pketsentsidis@gmail.com>
2015-10-27 20:42:30 +0200

    updated expand effect

--- calc101/expand.js
+++ calc101/expand.js
@@ -2,16 +2,20 @@
  * Created by Neo on 10/27/2015.
  */
 var expand = function() {
-    var status = document.getElementById("extras").style.display;
-    if(status !== "none") {
-        document.getElementById("extras").style.display = "none";
-        document.getElementById("inner_container").style.height = "";
-        document.getElementById("breakline").style.display = "none";
-    }
-    else
-    {
+    var status = document.getElementById("shorten").style.display;
+    if(status == "none"){
         document.getElementById("extras").style.display = "block";
         document.getElementById("inner_container").style.height = "550px";
         document.getElementById("breakline").style.display = "block";
+        document.getElementById("expand").style.display = "none";
+        document.getElementById("shorten").style.display = "block";
     }
+    else{
+        document.getElementById("extras").style.display = "none";
+        document.getElementById("inner_container").style.height = "";
+        document.getElementById("breakline").style.display = "none";
+        document.getElementById("expand").style.display = "block";
+        document.getElementById("shorten").style.display = "none";
+    }
+
 }
