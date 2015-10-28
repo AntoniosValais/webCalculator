b84ce0ca2a2d28d8d57a550f53f7f951d52921f8
TonyVkoz <toniovkozz@gmail.com>
2015-10-28 13:49:54 +0200

    Show result functionality

--- TeamCalc/jscalc.js
+++ TeamCalc/jscalc.js
@@ -1,61 +1,75 @@
-var  prod = [];
+var prod = [];
 
-function input(elements) {
-    var  newInputs = elements.innerHTML;
-    var  currentProd = prod;
+function input(elements)
+{
+    var newInputs = elements.innerHTML;
+    var currentProd = prod;
+
+    if( newInputs == '=')
+    {
+        var result = math.eval( currentProd.join(" "));
+        document.getElementById("screen").innerHTML = result;
+        return;
+    }
 
     prod.push(newInputs);
     prod = funckChecker(currentProd);
     var result = "";
-    for (var i = 0; i < prod.length; i++){
+    for (var i = 0; i < prod.length; i++)
+    {
         result += prod[i];
     }
     document.getElementById("screen").innerHTML = result;
 }
 
-function clearAll() {
+function clearAll()
+{
     document.getElementById("screen").innerHTML = "";
     prod = [];
 }
 
 
 /*
-Made by Tania
+ Made by Tania
 
-Dexetai enan pikana opou an sti teleytai thesi brisketai mathimatiki sinartisi
-tote bazei tin sinartisi stin arxi tou pinaka,an stin proteleutai thesi iparxei parenthesi
-tote kanei append tin sinartisi. 
+ Dexetai enan pikana opou an sti teleytai thesi brisketai mathimatiki sinartisi
+ tote bazei tin sinartisi stin arxi tou pinaka,an stin proteleutai thesi iparxei parenthesi
+ tote kanei append tin sinartisi.
 
-@param inputArray   Pinakas me input
-output ton allagmeno pinaka
+ @param inputArray   Pinakas me input
+ output ton allagmeno pinaka
 
-*/
-function funckChecker(inputArray){
-	
-	var lastInput = inputArray[inputArray.length-1];
-	var funcButtons = document.getElementsByClassName("mathFunc");
-	
-	for(var i=0;i<funcButtons.length;i++)
-	{
-		if(lastInput === funcButtons[i].innerHTML)
-		{
-			if(inputArray.length === 1)
-			{	//an sinartisi einai to proto input pou dothike
-				inputArray[inputArray.length-1] = lastInput+"(";
-			}else{	
-					if(inputArray[inputArray.length-2].match(/[^0-9\)]/))
-					{	//an prin tin sinartisi einai otidipote ekstos apo arithmous
-						//kai ) tote kane append tin sinartisi
-						inputArray[inputArray.length-1] = lastInput+"(";
-					}else{
-						//allios pane tin sinartisi stin arxi kai kleise parenthesi
-						inputArray.pop();
-						inputArray.unshift(lastInput+"(");
-						inputArray.push(")");
-					}
-			}
-		}
-	}
-	
-	return inputArray;
+ */
+function funckChecker(inputArray)
+{
+
+    var lastInput = inputArray[inputArray.length - 1];
+    var funcButtons = document.getElementsByClassName("mathFunc");
+
+    for (var i = 0; i < funcButtons.length; i++)
+    {
+        if (lastInput === funcButtons[i].innerHTML)
+        {
+            if (inputArray.length === 1)
+            {	//an sinartisi einai to proto input pou dothike
+                inputArray[inputArray.length - 1] = lastInput + "(";
+            }
+            else
+            {
+                if (inputArray[inputArray.length - 2].match(/[^0-9\)]/))
+                {	//an prin tin sinartisi einai otidipote ekstos apo arithmous
+                    //kai ) tote kane append tin sinartisi
+                    inputArray[inputArray.length - 1] = lastInput + "(";
+                } else
+                {
+                    //allios pane tin sinartisi stin arxi kai kleise parenthesi
+                    inputArray.pop();
+                    inputArray.unshift(lastInput + "(");
+                    inputArray.push(")");
+                }
+            }
+        }
+    }
+
+    return inputArray;
 }
