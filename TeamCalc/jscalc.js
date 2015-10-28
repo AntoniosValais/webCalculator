d4b61bcd6d61d213ff35f2c37e0c8c1625371a42
TonyVkoz <toniovkozz@gmail.com>
2015-10-28 21:26:14 +0200

    Controller Alpha

--- TeamCalc/jscalc.js
+++ TeamCalc/jscalc.js
@@ -1,14 +1,108 @@
-var prod = [];
+var controller = new CalculatorController();
 
+function CalculatorController()
+{
+    this.prod = [];
+    this.newInput = null;
+    this.result = 0;
+
+    this.receiveInputFrom = function receiveInputFrom( element )
+    {
+        this.newInput = element.title;
+
+        if( this.newInput == '=')
+        {
+            try
+            {
+                this.result = math.eval( this.prod.join(""));
+                document.getElementById("screen").innerHTML = this.result.toString();
+            }
+            catch(e)
+            {
+                document.getElementById("screen").innerHTML = "Expression Error!!!";
+            }
+            return;
+        }
+
+        this.prod.push( this.newInput );
+
+        this.funckChecker();
+
+        this.result = "";
+
+        for (var i = 0; i < this.prod.length; i++)
+        {
+            this.result += this.prod[i];
+        }
+
+        document.getElementById("screen").innerHTML = this.result;
+    }
+
+    /*
+     Made by Tania
+
+     Dexetai enan pikana opou an sti teleytai thesi brisketai mathimatiki sinartisi
+     tote bazei tin sinartisi stin arxi tou pinaka,an stin proteleutai thesi iparxei parenthesi
+     tote kanei append tin sinartisi.
+
+     */
+    this.funckChecker = function funckChecker()
+    {
+        var lastInput = this.prod[ this.prod.length - 1 ];
+        var funcButtons = document.getElementsByClassName("mathFunc");
+
+        for ( var i = 0; i < funcButtons.length; i++ )
+        {
+            if ( lastInput === funcButtons[i].title )
+            {
+                if( this.prod.length === 1 )
+                {	//an sinartisi einai to proto input pou dothike
+                    this.prod[ this.prod.length - 1 ] = lastInput + "(";
+                }
+                else
+                {
+                    if ( this.prod[ this.prod.length - 2 ].match( /[^0-9\)\'pi']/ ) )
+                    {	//an prin tin sinartisi einai otidipote ekstos apo arithmous
+                        //kai ) tote kane append tin sinartisi
+                        this.prod[ this.prod.length - 1 ] = lastInput + "(";
+                    }
+                    else
+                    {
+                        //allios pane tin sinartisi stin arxi kai kleise parenthesi
+                        this.prod.pop();
+                        this.prod.unshift(lastInput + "(");
+                        this.prod.push(")");
+                    }
+                }
+            }
+        }
+    }
+
+    this.clearAll = function clearAll()
+    {
+        document.getElementById("screen").innerHTML = "";
+        this.prod = [];
+    }
+}
+
+
+/*
 function input(elements)
 {
-    var newInputs = elements.innerHTML;
+    var newInputs = elements.title;
     var currentProd = prod;
 
     if( newInputs == '=')
     {
-        var result = math.eval( currentProd.join(""));
-        document.getElementById("screen").innerHTML = result;
+        try
+        {
+            var result = math.eval( currentProd.join(""));
+            document.getElementById("screen").innerHTML = result;
+        }
+        catch(e)
+        {
+            document.getElementById("screen").innerHTML = "Expression Error!!!";
+        }
         return;
     }
 
@@ -27,7 +121,7 @@ function clearAll()
     document.getElementById("screen").innerHTML = "";
     prod = [];
 }
-
+*/
 
 /*
  Made by Tania
@@ -40,6 +134,8 @@ function clearAll()
  output ton allagmeno pinaka
 
  */
+
+/*
 function funckChecker(inputArray)
 {
 
@@ -48,19 +144,20 @@ function funckChecker(inputArray)
 
     for (var i = 0; i < funcButtons.length; i++)
     {
-        if (lastInput === funcButtons[i].innerHTML)
+        if (lastInput === funcButtons[i].title)
         {
-            if (inputArray.length === 1)
+            if(inputArray.length === 1)
             {	//an sinartisi einai to proto input pou dothike
                 inputArray[inputArray.length - 1] = lastInput + "(";
             }
             else
             {
-                if (inputArray[inputArray.length - 2].match(/[^0-9\)]/))
+                if ( inputArray[inputArray.length - 2].match( /[^0-9\)\'pi']/ ) )
                 {	//an prin tin sinartisi einai otidipote ekstos apo arithmous
                     //kai ) tote kane append tin sinartisi
                     inputArray[inputArray.length - 1] = lastInput + "(";
-                } else
+                }
+                else
                 {
                     //allios pane tin sinartisi stin arxi kai kleise parenthesi
                     inputArray.pop();
@@ -73,3 +170,4 @@ function funckChecker(inputArray)
 
     return inputArray;
 }
+*/
