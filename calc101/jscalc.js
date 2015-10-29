c9af594ba73da1ffda0662fe7b15563c560f12c9
iKetsi <pketsentsidis@gmail.com>
2015-10-29 17:51:31 +0200

    inc math.js /jscalc.js updates from teamcalc

--- calc101/jscalc.js
+++ calc101/jscalc.js
@@ -1,58 +1,173 @@
-/**
- * Created by Neo on 10/25/2015.
- */
-var numb1,numb2,operFlag;
-var calculate = function(numb1,numb2,operFlag){
-    var result;
-    if(operFlag ==('+')){
-        result = numb1 + numb2;
+var controller = new CalculatorController();
+
+function CalculatorController()
+{
+    this.prod = [];
+    this.newInput = null;
+    this.result = 0;
+
+    this.receiveInputFrom = function receiveInputFrom( element )
+    {
+        this.newInput = element.title;
+
+        if( this.newInput == '=')
+        {
+            try
+            {
+                this.result = math.eval( this.prod.join(""));
+                document.getElementById("screen").innerHTML = this.result.toString();
+            }
+            catch(e)
+            {
+                document.getElementById("screen").innerHTML = "Expression Error!!!";
+            }
+            return;
+        }
+
+        this.prod.push( this.newInput );
+
+        this.funckChecker();
+
+        this.result = "";
+
+        for (var i = 0; i < this.prod.length; i++)
+        {
+            this.result += this.prod[i];
+        }
+
+        document.getElementById("screen").innerHTML = this.result;
     }
-    else if(operFlag ==('-')){
-        result = numb1 - numb2;
+
+    /*
+     Made by Tania
+
+     Dexetai enan pikana opou an sti teleytai thesi brisketai mathimatiki sinartisi
+     tote bazei tin sinartisi stin arxi tou pinaka,an stin proteleutai thesi iparxei parenthesi
+     tote kanei append tin sinartisi.
+
+     */
+    this.funckChecker = function funckChecker()
+    {
+        var lastInput = this.prod[ this.prod.length - 1 ];
+        var funcButtons = document.getElementsByClassName("mathFunc");
+
+        for ( var i = 0; i < funcButtons.length; i++ )
+        {
+            if ( lastInput === funcButtons[i].title )
+            {
+                if( this.prod.length === 1 )
+                {	//an sinartisi einai to proto input pou dothike
+                    this.prod[ this.prod.length - 1 ] = lastInput + "(";
+                }
+                else
+                {
+                    if ( this.prod[ this.prod.length - 2 ].match( /[^0-9\)\'pi']/ ) )
+                    {	//an prin tin sinartisi einai otidipote ekstos apo arithmous
+                        //kai ) tote kane append tin sinartisi
+                        this.prod[ this.prod.length - 1 ] = lastInput + "(";
+                    }
+                    else
+                    {
+                        //allios pane tin sinartisi stin arxi kai kleise parenthesi
+                        this.prod.pop();
+                        this.prod.unshift(lastInput + "(");
+                        this.prod.push(")");
+                    }
+                }
+            }
+        }
     }
-    else if(operFlag ==('*')){
-        result = numb1 * numb2;
+
+    this.clearAll = function clearAll()
+    {
+        document.getElementById("screen").innerHTML = "";
+        this.prod = [];
     }
-    else if(operFlag ==('/')){
-        result = numb1 / numb2;
-    }
-    return result;
 }
 
-var input = function(x) {
-    if (x == 'C') {
-        document.getElementById("screen").innerHTML = "";
-    }
-    else if (x == '+') {
-        numb1 = document.getElementById("screen").innerHTML;
-        document.getElementById("screen").innerHTML = x;
-        operFlag = '+';
-    }
-    else if (x == '-') {
-        numb1 = document.getElementById("screen").innerHTML;
-        document.getElementById("screen").innerHTML = x;
-        operFlag = '-';
-    }
-    else if (x == '*') {
-        numb1 = document.getElementById("screen").innerHTML;
-        document.getElementById("screen").innerHTML = x;
-        operFlag = '*';
-    }
-    else if (x == '/') {
-        numb1 = document.getElementById("screen").innerHTML;
-        document.getElementById("screen").innerHTML = x;
-        operFlag = '/';
-    }
-    else if (x == '=') {
-        numb2 = document.getElementById("screen").innerHTML;
-        result = calculate(numb1, numb2, operFlag);
-        document.getElementById("screen").innerHTML = result;
-    }
-    else if (x == '0' || x == '1'|| x == '2'|| x == '3'|| x == '4'|| x == '5'|| x == '6'|| x == '7'|| x == '8'|| x == '9') {
-        if(document.getElementById("screen").innerHTML !== '*' || '+' || '-' || '/' || '=')
-        {document.getElementById("screen").innerHTML += x;}
-        else
-            document.getElementById("screen").innerHTML += x;
-    }
 
-};
+/*
+ function input(elements)
+ {
+ var newInputs = elements.title;
+ var currentProd = prod;
+
+ if( newInputs == '=')
+ {
+ try
+ {
+ var result = math.eval( currentProd.join(""));
+ document.getElementById("screen").innerHTML = result;
+ }
+ catch(e)
+ {
+ document.getElementById("screen").innerHTML = "Expression Error!!!";
+ }
+ return;
+ }
+
+ prod.push(newInputs);
+ prod = funckChecker(currentProd);
+ var result = "";
+ for (var i = 0; i < prod.length; i++)
+ {
+ result += prod[i];
+ }
+ document.getElementById("screen").innerHTML = result;
+ }
+
+ function clearAll()
+ {
+ document.getElementById("screen").innerHTML = "";
+ prod = [];
+ }
+ */
+
+/*
+ Made by Tania
+
+ Dexetai enan pikana opou an sti teleytai thesi brisketai mathimatiki sinartisi
+ tote bazei tin sinartisi stin arxi tou pinaka,an stin proteleutai thesi iparxei parenthesi
+ tote kanei append tin sinartisi.
+
+ @param inputArray   Pinakas me input
+ output ton allagmeno pinaka
+
+ */
+
+/*
+ function funckChecker(inputArray)
+ {
+
+ var lastInput = inputArray[inputArray.length - 1];
+ var funcButtons = document.getElementsByClassName("mathFunc");
+
+ for (var i = 0; i < funcButtons.length; i++)
+ {
+ if (lastInput === funcButtons[i].title)
+ {
+ if(inputArray.length === 1)
+ {	//an sinartisi einai to proto input pou dothike
+ inputArray[inputArray.length - 1] = lastInput + "(";
+ }
+ else
+ {
+ if ( inputArray[inputArray.length - 2].match( /[^0-9\)\'pi']/ ) )
+ {	//an prin tin sinartisi einai otidipote ekstos apo arithmous
+ //kai ) tote kane append tin sinartisi
+ inputArray[inputArray.length - 1] = lastInput + "(";
+ }
+ else
+ {
+ //allios pane tin sinartisi stin arxi kai kleise parenthesi
+ inputArray.pop();
+ inputArray.unshift(lastInput + "(");
+ inputArray.push(")");
+ }
+ }
+ }
+ }
+
+ return inputArray;
+ }
+ */
